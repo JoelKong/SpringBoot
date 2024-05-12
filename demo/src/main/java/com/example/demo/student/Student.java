3f98fca034b9e0bb29f75e96bf11b3a22a62e468
Joel Kong <joelkong2001@gmail.com>
2024-05-12 15:46:58 +0800

    chore: integrated postgres and mapped class to table using jpa

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -1,9 +1,16 @@
 package com.example.demo.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
     // Student attributes
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private Long id;
     private String name;
     private String email;
